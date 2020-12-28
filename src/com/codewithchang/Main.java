65ef740957a9b0260e6db73a6446d619efadc685
Chang <cnxiong03@gmail.com>
2020-12-27 20:35:55 -0500

    casting

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         int myValue = 1000;
 
-        int myMinValue = Integer.MIN_VALUE;
-        int myMaxValue = Integer.MAX_VALUE;
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
         System.out.println("Integer Minimum Value = " + myMinValue);
         System.out.println("Integer Maximum Value = " + myMaxValue);
         System.out.println("Busted MAX value = " + (myMaxValue + 1));
@@ -31,5 +31,14 @@ public class Main {
         System.out.println("Long Maximum Value = " + myMaxLongValue);
         long bigLongLiteralValue = 2_147_483_647_234L;
         System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
+
+        int myTotal = (myMinIntValue / 2);
+
+        // Casting
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
+
+        short myNewShortValue = (short) (myMinShortValue / 2);
     }
 }
